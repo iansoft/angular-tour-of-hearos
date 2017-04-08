e3655b75bce95e9294fea17614c7c3b5e65730d3
iansoft <anziloveanzi@hotmail.com>
2017-04-08 22:50:57 +0800

    add the hero form

--- src/app/hero-form.component.ts
+++ src/app/hero-form.component.ts
@@ -16,6 +16,10 @@ export class HeroFormComponent {
 
     onSubmit() { this.submitted = true; }
 
+    newHero() {
+      this.model = new Hero2(42, '', '');
+    }
+
     // TODO: Remove this when we're done
     // 增加diagnostic属性，它返回这个模型的 JSON 形式。
     // 在开发过程中，它用于调试，最后清理时会丢弃它。
